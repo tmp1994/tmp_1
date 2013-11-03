f18fa9bc7fdbdedd9d2d2d182b6998e598119944
Vitaly <vitali0@me.com>
2013-11-03 18:26:45 +0400

    1.1

---
+++ main_3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+typedef unsigned long uLong;
+
+int main() {
+	int n = 0;
+
+	cin >> n;
+
+	uLong *id = new uLong[n];
+	map<uLong, uLong> res;
+
+	for (uLong i = 0; i < n; i++) {
+		cin >> id[i];
+		auto entry = make_pair(id[i], 1);
+		auto tt = res.insert(entry);
+		if (!tt.second) {
+			res[id[i]]++;
+		}
+	}
+	
+	int result = 0;
+
+	for (auto iter = res.begin(); iter != res.end(); iter++) {
+		if (iter->first != 0)
+		{
+			if (iter->second == 2) {
+				result++;
+			} else {
+				if (iter->second > 2) {
+					result = -1;
+					break;
+				}
+			}
+		}
+	}
+
+	cout << result << endl;
+}
